8fa7341cefd91cd02e4467d8ffdf2ef0162fccc7
NitinNb9393 <nitinbabar9393@gmail.com>
2021-10-28 17:54:27 +0530

    Added: Program for check equality of both line

--- src/com/birdgelabe/LineComparison/LineComparison.java
+++ src/com/birdgelabe/LineComparison/LineComparison.java
@@ -3,10 +3,13 @@ package com.birdgelabe.LineComparison;
 import java.util.Scanner;
 
 public class LineComparison {
+	public static int x, x1, x2, x3, y, y1, y2, y3;
+	public static double distance1, distance2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line comparison program ");
 		input();
+		distance();
 	}
 
 	public static void input() {
@@ -22,5 +25,13 @@ public class LineComparison {
 
 		double distance = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
 		System.out.println("Distance = " + distance);
+	}
+
+	public static void distance() {
+		distance1 = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+		System.out.println("Distance = " + distance1);
+		distance2 = Math.sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+		System.out.println("Distance = " + distance2);
+
 	}
 }
